f6be47a4c697a08a19c8b2e1ad77d8788ce593e5
Leandro Jesús Rodríguez <124853303+LeandroJess@users.noreply.github.com>
2023-03-22 08:55:17 -0400

    Add files via upload

---
+++ fghtdyher.py
@@ -0,0 +1,9 @@
+num1 = float(input("Ingrese el primer número"))
+num2 = float(input("Ingrese el segundo número"))
+num3 = float(input("Ingrese el tercer número"))
+    if num1 > nu2 and num1 > 3:
+        print("El primer número es mayor", num1):
+            elif num2 > num1 and num2 > num3:
+                print("El segundo número es mayor", num2)
+
+
